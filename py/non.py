12ae5b9c31fbb7ce7e0206ed84b539e337afa8f3
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2023-09-16 15:54:31 +0700

    del
    del

--- py/non.py
+++
@@ -1,9 +0,0 @@
-"""Inflation"""
-def main():
-    """Inflation"""
-    num_n = int(float(input())*100)
-    num_k = int(input())
-    for _ in range(num_k):
-        num_n = num_n + num_n*381//10000
-    print("%d.%02d"%(num_n//100, num_n%100))
-main()
