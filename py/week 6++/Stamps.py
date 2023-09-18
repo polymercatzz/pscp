6a1f64584dff959c190152d9bb9f463e3d6d1a99
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2023-09-18 17:40:21 +0700

    stamps

---
+++ py/week 6++/Stamps.py
@@ -0,0 +1,17 @@
+"""Stamps"""
+import math
+def main(time, pro, stamp_b, stamp_c, discount):
+    """Stamps"""
+    stamp = 0
+    result = 0
+    for _ in range(time):
+        price = int(input())
+        if stamp >= stamp_c:
+            dis = min(price, discount*(stamp//stamp_c))
+            price -= dis
+            stamp -= math.ceil(dis/discount)*stamp_c
+        if price >= pro:
+            stamp += stamp_b*(price//pro)
+        result += price
+    print(result, stamp, sep="\n")
+main(int(input()), int(input()), int(input()), int(input()), int(input()))
