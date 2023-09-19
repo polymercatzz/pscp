5fc09c6d10a3d265c4494d708a2db1f2a3333616
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2023-09-19 21:24:07 +0700

    pscp

--- py/week 6++/FourDirections.py
+++ py/week 6++/FourDirections.py
@@ -1,6 +1,28 @@
 """FourDirections"""
-def main():
-    word = input()
-    print("  *  ")*len(word)
-    for i in word:
-        if i == "U"
+def main(line1="", line2="", line3="", line4=""):
+    """main"""
+    direct = input()
+    print("  *   "*len(direct))
+    for i in direct:
+        if i == "U":
+            line1 += " ***  "
+            line2 += "* * * "
+            line3 += "  *   "
+            line4 += "  *   "
+        elif i == "D":
+            line1 += "  *   "
+            line2 += "* * * "
+            line3 += " ***  "
+            line4 += "  *   "
+        elif i == "L":
+            line1 += " *    "
+            line2 += "***** "
+            line3 += " *    "
+            line4 += "  *   "
+        elif i == "R":
+            line1 += "   *  "
+            line2 += "***** "
+            line3 += "   *  "
+            line4 += "  *   "
+    print(line1, line2, line3, line4, sep="\n")
+main()
--- py/week 6++/Milk.py
+++ py/week 6++/Milk.py
@@ -3,10 +3,9 @@ def main(price, faa, free, money):
     """[Recommend] Milk"""
     bottle = money//price
     result = bottle
-    if bottle >= faa and faa != 0:
-        while bottle >= faa:
-            pro_free = (bottle//faa)
-            result += pro_free*free
-            bottle = bottle-pro_free*faa+pro_free*free
+    while bottle >= faa and faa != 0:
+        pro_free = (bottle//faa)
+        result += pro_free*free
+        bottle = bottle-pro_free*faa+pro_free*free
     print(result)
 main(int(input()), int(input()), int(input()), int(input()))
