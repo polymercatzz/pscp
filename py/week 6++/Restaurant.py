6752cfce891cca9c8dbd882b53fa5a1a564fc8b4
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2023-09-19 21:58:50 +0700

    pscp

--- py/week 6++/Restaurant.py
+++ py/week 6++/Restaurant.py
@@ -6,14 +6,14 @@ def main():
     percen = float(input())
     food = float(input())
     buy_food = cost_food+food
-    discout = 0
+    discout = buy_food
     if cost_food == promo:
         cost_food = cost_food*(1-(percen/100))
     if buy_food >= promo:
         discout = buy_food*(1-(percen/100))
-    if cost_food-discout > 0 and food != 0 and percen != 100:
+    if discout < cost_food:
         print("Yes %.3f"%(abs(discout-cost_food)))
-    elif cost_food-discout < 0:
+    elif discout > cost_food:
         print("No %.3f"%(abs(discout-cost_food)))
     else:
         print("Yes")
