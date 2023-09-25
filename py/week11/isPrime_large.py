1a5bc898a013f1223e5e3a558b7c3f8a5a15396c
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2023-09-25 21:33:21 +0700

    pscp

--- py/week11/isPrime_large.py
+++ py/week11/isPrime_large.py
@@ -2,20 +2,20 @@
 def prime():
     """isPrime_large"""
     num = int(input())
-    result = "False"
+    result = "NO"
     if num <= 1:
-        result = "False"
+        result = "NO"
     elif num <= 3:
-        result = "True"
+        result = "YES"
     elif num % 2 == 0 or num % 3 == 0:
-        result = "False"
+        result = "NO"
     else:
         test = 5
         while test * test <= num:
             if num % test == 0 or num % (test+2) == 0:
-                result = "False"
+                result = "NO"
                 break
             test += 6
-            result = "True"
+            result = "YES"
     print(result)
 prime()
