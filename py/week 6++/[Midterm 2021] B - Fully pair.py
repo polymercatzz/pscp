8abac000904679e8a5f5c73725b77ff20dfa15ef
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2023-09-28 23:57:58 +0700

    pscp

--- py/week 6++/[Midterm 2021] B - Fully pair.py
+++ py/week 6++/[Midterm 2021] B - Fully pair.py
@@ -1,13 +1,11 @@
 """[Midterm 2021] B - Fully pair?"""
 def main(word):
     """[Midterm 2021] B - Fully pair?"""
-    count = ""
     result = ""
     for i in word:
-        if i not in count:
+        if i not in result:
             if word.count(i)%2 == 1:
                 result += i
-            count += i
     if result == "":
         print("fully paired")
     else:
